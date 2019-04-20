7ac2d02f9b2ed3a2a8fcf5007e331d38d3a0953c
Geraldo Castro <victormatheuscastro@gmail.com>
2019-04-20 01:10:34 -0300

    Save documents (urls) in mongodb

--- run.py
+++ run.py
@@ -1,6 +1,31 @@
-from crawler import run_crawler
+from crawler import crawler
 from loguru import logger
+from mongo import save_links
 import os
+
+
+def run_crawler(url, allows_foreign_urls=False):
+    deque_urls = deque([url])
+    broken_urls = []
+    all_links = {}
+    while deque_urls:
+        url = deque_urls.popleft()
+        if url in broken_urls:
+            continue
+        crawled_data = crawler(url)
+        if allows_foreign_urls:
+            deque_urls += deque(crawled_data)
+            broken_urls += crawled_data['broken_urls']
+        all_links[crawled_data['base_url']] = crawled_data['']
+        save_links(
+            crawled_data['base_url'],
+            {   
+                'base_url': crawled_data['base_url'],
+                'processed_urls': crawled_data['processed_urls']
+            }
+        )
+    return all_links
+
 
 
 if __name__ == "__main__":
