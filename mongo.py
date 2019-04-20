cf2c567a69a9a71220e95f7dc7a3b393642bcb3a
Geraldo Castro <victormatheuscastro@gmail.com>
2019-04-20 01:09:05 -0300

    Rename dict field

--- mongo.py
+++ mongo.py
@@ -2,11 +2,11 @@ from pymongo import MongoClient
 
 client = MongoClient('mongodb://localhost:27017/')
 
-def save_links(base, data, db_name='crawler', collection_name='urls')
+def save_links(base_url, data, db_name='crawler', collection_name='urls')
     db = client[db_name]
     collection = db[collection_name]
     document = collection.update(
-        {'base': base},
+        {'base_url': base_url},
         {'$set': data},
         True
     )
