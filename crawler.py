efc812b8a6a5064e76a33dd543640edb9dc19399
Geraldo Castro <victormatheuscastro@gmail.com>
2019-04-21 02:45:13 -0300

    Group foreign links by url_base

--- crawler.py
+++ crawler.py
@@ -8,7 +8,7 @@ def crawler(url):
     deque_urls = deque([url])
     processed_urls = set()
     broken_urls = set()
-    foreign_urls = set()
+    foreign_urls = dict()
 
     while deque_urls:
         local_urls = set()
@@ -29,11 +29,16 @@ def crawler(url):
         links = response.html.absolute_links
 
         for link in links:
-            if not link in processed_urls and not link in deque_urls:
+            if not (link in processed_urls) and not (link in deque_urls):
                 if link.startswith(base_url):
                     local_urls.add(link)
                 else:
-                    foreign_urls.add(link)
+                    foreign_splited = urlsplit(link)
+                    foreign_base = f'{foreign_splited.scheme}://{foreign_splited.netloc}'
+                    if foreign_base in foreign_urls:
+                        foreign_urls[foreign_base].append(link)
+                    else:
+                        foreign_urls[foreign_base] = [link]
 
         logger.info(f'Local: {len(local_urls)}')
         logger.info(f'Foreign: {len(foreign_urls)}')
@@ -44,7 +49,7 @@ def crawler(url):
         'base_url': base_url,
         'processed_urls': list(processed_urls),
         'broken_urls': list(broken_urls),
-        'foreign_urls': list(foreign_urls)
+        'foreign_urls': foreign_urls
     }
     logger.info(f'Processed: {len(result_data["processed_urls"])} ({len(result_data["broken_urls"])} broken)')
     return result_data
