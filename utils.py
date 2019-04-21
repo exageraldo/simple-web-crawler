9b7c65346c5f2c68530072716212ea614d84cc83
Geraldo Castro <victormatheuscastro@gmail.com>
2019-04-21 12:27:29 -0300

    Add utils

---
+++ utils.py
@@ -0,0 +1,12 @@
+from urllib.parse import urlsplit, urlparse
+
+
+def remove_fragment(link):
+    parts = urlsplit(link)
+    if parts.fragment:
+        link = f'{parts.scheme}//{parts.netloc}{parts.path}'
+    return link
+
+def get_base_url(url):
+    parts = urlsplit(url)
+    return f'{parts.scheme}://{parts.netloc}'
