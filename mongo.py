aa9be47a78eb42051d723fa68cb7fc1bb1fc8d02
Geraldo Castro <victormatheuscastro@gmail.com>
2019-04-20 21:55:05 -0300

    Fix function and configure mongo url

--- mongo.py
+++ mongo.py
@@ -1,8 +1,8 @@
 from pymongo import MongoClient
 
-client = MongoClient('mongodb://localhost:27017/')
+client = MongoClient('mongodb://root:strong-password@mongodb:27017/')
 
-def save_links(base_url, data, db_name='crawler', collection_name='urls')
+def save_links(base_url, data, db_name='crawler', collection_name='urls'):
     db = client[db_name]
     collection = db[collection_name]
     document = collection.update(
