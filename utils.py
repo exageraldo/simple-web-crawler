c7b2a2f4910d04dc53db0719f4934a63b2c42b99
Geraldo Castro <victormatheuscastro@gmail.com>
2019-04-21 16:12:23 -0300

    Fix url in string format

--- utils.py
+++ utils.py
@@ -1,11 +1,12 @@
-from urllib.parse import urlsplit, urlparse
+from urllib.parse import urlsplit
 
 
 def remove_fragment(link):
     parts = urlsplit(link)
     if parts.fragment:
-        link = f'{parts.scheme}//{parts.netloc}{parts.path}'
+        link = f'{parts.scheme}://{parts.netloc}{parts.path}'
     return link
+
 
 def get_base_url(url):
     parts = urlsplit(url)
