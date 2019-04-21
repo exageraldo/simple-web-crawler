c8f31248edcaa6cb0d881051606f0dd6174117f5
Geraldo Castro <victormatheuscastro@gmail.com>
2019-04-21 16:11:42 -0300

    Save foreign links in db

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ from crawler import crawler
 from loguru import logger
 from mongo import save_links
 import os
+from datetime import datetime
 from collections import deque
 
 
@@ -23,7 +24,8 @@ def run_crawler(url, allows_foreign_urls=False):
             {   
                 'base_url': crawled_data['base_url'],
                 'processed_urls': crawled_data['processed_urls'],
-                'foreign_urls': crawled_data['foreign_urls']
+                'foreign_urls': crawled_data['foreign_urls'],
+                'updated_at': datetime.now()
             }
         )
     return all_links
